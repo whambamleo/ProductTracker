6bf04101b1a7d958c6d240eac35418177f431c1c
EC2 Default User <ec2-user@ip-172-31-18-52.ec2.internal>
2024-06-13 19:55:01 +0000

    Project initialized, working on EC2 instance

---
+++ src/main/java/com/whambamleo/GradleTutorial.java
@@ -0,0 +1,7 @@
+package com.whambamleo;
+
+public class GradleTutorial {
+	public static void main(String[] args) {
+		System.out.println("Hello, Gradle!");
+	}
+}
---
+++ src/test/java/com/whambamleo/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.whambamleo;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+        @Test
+        public void simpleTest() {
+                GradleTutorial.main(new String[]{});
+        }
+
+}
